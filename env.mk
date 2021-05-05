b9bf3e5ee7d0b96c50743619471c03e708f7bb29
David.Gao <david.alpha.fox@gmail.com>
2021-05-05 15:04:39 +0800

    examples

---
+++ env.mk
@@ -0,0 +1,3 @@
+ERTS_INCLUDE_DIR ?= /usr/local/Cellar/erlang/23.2.7/lib/erlang/erts-11.1.8/include/
+ERL_INTERFACE_INCLUDE_DIR ?= /usr/local/Cellar/erlang/23.2.7/lib/erlang/lib/erl_interface-4.0.2/include
+ERL_INTERFACE_LIB_DIR ?= /usr/local/Cellar/erlang/23.2.7/lib/erlang/lib/erl_interface-4.0.2/lib
